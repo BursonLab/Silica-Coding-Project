c05e42b018a5b98453d3b445d62dc19322358686
Thomas Marsh <tmarsh@hamilton.edu>
2017-10-04 23:08:38 -0400

    Added Hole Finding
    Added hole finding and the ability to find the distances from every center to the nearest point on a hole
    
    Added ability to plot multiple histograms to compare the distribution of ring sizes as you get further from holes (using custom bin sizes)
    
    Improved performance for large images with less defined rings
    
    Cleaned up code and added more documentation of how it works
    
    Increased encapsulation of code to assist in integration with other scripts

--- centerFinder.py
+++ centerFinder.py
@@ -8,42 +8,132 @@ from skimage import draw
 from skimage import util
 from skimage import color
 from skimage import viewer
+from skimage import morphology
+from skimage import filters
+from skimage import measure
+from skimage import transform
 
 from sklearn.neighbors import NearestNeighbors
 
+from scipy import ndimage as ndi
 
-filename = "SampleImage.jpg"#"STM.jpg"#"connectionPoint.png"
+import matplotlib.pyplot as plt
+
+
+filename = "HolesImageLarge.jpg"
+
+light_centers = False
 
 #Dimensions of image [width, height] in nm
 dimensions = [9.3, 5.6]
+#Number of holes in the image
+num_holes = 2
 
 #Outputs
 save_image = False
-output_filename = "Output.png"
+output_filename = "LargeImage.png"
 save_xyz_file = False
 xyz_output_filename = "Output.txt"
 
-#Key Image
-has_key = False
-key_filename = "connectionPointKey.png"
 
 display_result = True
 
 #Colors for each of the ring numbers
 colors = [[178, 112, 248], [75, 176, 246], [67, 196, 127], [249, 222, 62], [249, 76, 62], [247, 38, 232]]
 
-image = io.imread(filename)
-image_copy = io.imread(filename)
-key_image = io.imread(key_filename)
+scaling_factor = 1
+
+#Imports and scales the image by a given scaling factor
+def importAndScale(filename):
+    original = io.imread(filename)
+    return transform.rescale(original, scaling_factor, mode='constant', preserve_range=True).astype('uint8')
+
+image = importAndScale(filename)
+image_copy = importAndScale(filename)
 
 #Convert image to greyscale 
 grey = color.rgb2grey(image)
 
-#Invert image
-grey_inv = util.invert(grey)
+#Invert image if centers are dark, else keep same
+if light_centers:
+    grey_inv = grey
+else:
+    grey_inv = util.invert(grey)
 
 image_width = len(grey)
 image_height = len(grey[0])
+
+#View image in separate viewer
+def viewImage(image):
+    view = viewer.ImageViewer(image)
+    view.show()
+    return view
+
+#Get coordinates of borders of holes from greyscale image
+def getHoleCoords(greyscale, num_holes):
+    #Threshold greyscale image to create a binary image
+    im_thresh = filters.threshold_minimum(greyscale)
+    binary = greyscale > im_thresh
+    
+    #Erode the binary image and then subtract from original to get borders
+    erosion = numpy.pad(morphology.binary_erosion(binary)[2:-2,2:-2],2,'maximum')
+    borders = binary ^ erosion
+    borders = numpy.pad(borders[1:-1,1:-1],1,'edge')
+    
+    #Label the regions in the image
+    label_image = measure.label(borders)
+    regions = measure.regionprops(label_image)
+    
+    #Put all of the areas of regions in border image into list
+    areas = []
+    for region in regions:
+        areas.append(region.area)
+
+    #Get the coordinates of the largest num_holes number of border regions
+    hole_coords = []
+    for i in range(num_holes):
+        max_ind = numpy.argmax(areas)
+        coords = regions[max_ind].coords
+        for coord in coords:
+            hole_coords.append([coord[1],coord[0]])
+            
+        del regions[max_ind]
+        del areas[max_ind]
+    
+    return hole_coords
+
+#Takes the coordinates of the borders of the holes and fills them in to get
+# a mask image of the holes
+def getHoleImage(hole_coords):
+    hole_image = numpy.zeros((image_width,image_height))
+    
+    for coord in hole_coords:
+        hole_image[coord[1],coord[0]] = 1
+
+    return ndi.binary_fill_holes(morphology.binary_closing(hole_image))
+
+hole_coords = getHoleCoords(grey, num_holes)
+
+holes = getHoleImage(hole_coords)
+
+#viewImage(holes)
+
+#Black out the holes in the greyscale image so no centers will be placed there
+grey_inv = grey_inv * (1 - holes)
+
+#Plots circles of a given radius and color on a given image at given coordinates
+def plotCirclesOnImage(image, coords, radius, color):
+    for coord in coords:
+        rr, cc = draw.circle(coord[1], coord[0], radius, shape=(image_width, image_height))
+        image[rr, cc] = color
+        
+        
+#Finds the distances and indices of the nearest given number of the base coords
+# to each of the provided search coords
+def getNearestNeighbors(base_coords, search_coords, num_neighbors):
+    nearest = NearestNeighbors(n_neighbors=num_neighbors, algorithm='ball_tree').fit(base_coords)
+    dist, ind = nearest.kneighbors(search_coords)
+    return dist, ind
 
 #Returns a list of the centers of the blobs
 def getBlobCenters(blobs):
@@ -58,36 +148,38 @@ def getBlobCenters(blobs):
     return centers
     
 #Find blobs in image in order to locate ring centers
-blobs = feature.blob_dog(grey_inv, min_sigma=0.03, max_sigma=30, sigma_ratio=2.8, threshold=0.8, overlap=0.5)
 
+# min_sigma -> increase to find more small rings
+# max_sigma -> increase to find more large rings
+# sigma_ratio -> used in calculation of rings
+# threshold -> decrease to detect less intense rings
+# overlap -> fraction of the blobs that are allowed to overlap with each other
+
+blobs = feature.blob_dog(grey_inv, min_sigma=0.07, max_sigma=30, sigma_ratio=2.8, threshold=0.55, overlap=0.26)
 centers = getBlobCenters(blobs)
 
 #Find the average distance to closest neighbor
-closest_pair = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(centers)
-c_dist, c_ind = closest_pair.kneighbors(centers)
+c_dist, c_ind = getNearestNeighbors(centers, centers, 2)
 average_closest = numpy.median(c_dist[:][1])
 
 #Blackout regions around already found ring centers
 average_thresh = 1.4
-for center in centers:
-    rr, cc = draw.circle(center[1], center[0], average_closest*average_thresh, shape=(image_width, image_height))
-    grey_inv[rr, cc] = 0
-    
+
+plotCirclesOnImage(grey_inv, centers, average_closest*average_thresh, 0)
+
 #Find blobs that may be rings that have not been found on first pass
 blobs = numpy.concatenate((blobs,feature.blob_dog(grey_inv, min_sigma=1, max_sigma=30, sigma_ratio=2.8, threshold=1.1, overlap=0.5)))
-
 centers = getBlobCenters(blobs)
 
+#Converts pixel coordinates to nm coordinates based on the dimensions of the image
 def pixelsToNm(pixel_coord, nm_dim, im_width, im_height):
     x_scale = nm_dim[0]/im_width
     y_scale = nm_dim[1]/im_height
     
     return [pixel_coord[0]*x_scale, pixel_coord[1]*y_scale]
 
-for center in centers:
-    rr, cc = draw.circle(center[1], center[0], 10, shape=(image_width, image_height))
-    image_copy[rr, cc] = [0,0,250]
-
+#Plot preliminary center locations on image
+plotCirclesOnImage(image_copy, centers, 10, [0, 0, 250])
 
 def onclick(event):
     #Remove the center that was clicked
@@ -109,18 +201,18 @@ cid = point_select.canvas.mpl_connect('button_press_event', onclick)
 
 point_select.show()
 
-#fig.show()
-
 #Get distances and indices of 9 nearest neighbors to every center
-neighbors = NearestNeighbors(n_neighbors=10, algorithm='ball_tree').fit(centers)
-distances, indices = neighbors.kneighbors(centers)
+distances, indices = getNearestNeighbors(centers, centers, 10)
 
+#Gets the distances from every center to the nearest point on the edge of a hole
+hole_distances, hole_inds = getNearestNeighbors(hole_coords, centers, 2)
 
+hole_dist = []
+ring_size = []
+center_coord = []
+
+#Threshold for the maximum distance that two centers can be apart to be concidered neighbors
 thresh = 1.35
-draw_lines = True
-
-if save_xyz_file:
-    text_file = open(xyz_output_filename, "w")
 
 for k in range(len(distances)):
     n_dists = distances[k]
@@ -147,39 +239,62 @@ for k in range(len(distances)):
     # ring neighbors were visible
     scaled_num_neighbors = int(num_neighbors * percent_visible)
     
+    exclude_dist = 50
+    
     #Gets the coordinates of the circles around the centers
-    r_out, c_out = draw.circle(centers[k][1], centers[k][0], 12, shape=(image_width, image_height))
-    rr, cc = draw.circle(centers[k][1], centers[k][0], 9, shape=(image_width, image_height))
-    
-    
-    if scaled_num_neighbors <= 9 and percent_visible < 1.8:
-        #Draws lines connecting a point to its neighbors
-        if draw_lines:
-            for i in range(1, scaled_num_neighbors):
-                r_line, c_line = draw.line(centers[k][1], centers[k][0], centers[indices[k][i]][1], centers[indices[k][i]][0])
-                try:
-                    image[r_line, c_line] = [250, 250, 250]
-                except IndexError:
-                    pass
+    if 4 <= scaled_num_neighbors <= 9 and percent_visible < 1.8 and hole_distances[k][1] > exclude_dist:
+        hole_dist.append(hole_distances[k][1])
+        ring_size.append(scaled_num_neighbors)
+        center_coord.append(centers[k])
+        
+        #Get circle coordinates for outlines and actual circles for centers
+        r_out, c_out = draw.circle(centers[k][1], centers[k][0], 15, shape=(image_width, image_height))
+        rr, cc = draw.circle(centers[k][1], centers[k][0], 12, shape=(image_width, image_height))
                 
+        #Plot outlines
         image[r_out, c_out] = [0, 0, 0]
         
         #Assign appropriate colors to center coordinates
         image[rr, cc] = colors[scaled_num_neighbors-4]
-        
-        if save_xyz_file:
-            text_file.write(str(scaled_num_neighbors) + ' '+ str(centers[k][0]) + ' ' + str(centers[k][1]) + ' 0\n')
 
-if save_xyz_file:
+#Outputs the center data in xyz file format
+def createXyzFile(center_coord, ring_size):
+    text_file = open(xyz_output_filename, "w")
+    
+    for i in range(len(ring_size)):
+        text_file.write(str(ring_size[i]) + ' '+ str(center_coord[i][0]) + ' ' + str(center_coord[i][1]) + ' 0\n')
+    
     text_file.close()
+    
+#Splits the ring size and hole distance data into bins based on 
+# a given bin size and plots histograms of all of the bins 
+def splitRingsIntoBins(bin_size, hole_dist, ring_size):
+    bin_list = []
+    bin_labels = []
+    
+    max_dist = int(numpy.amax(hole_dist))
+    
+    for k in range(0, max_dist, bin_size):
+        bin_labels.append(str(k) + '-' + str(k + bin_size))
+        
+        cur_bin = []
+        for i in range(len(hole_dist)):
+            if k <= hole_dist[i] < k + bin_size:
+                cur_bin.append(ring_size[i])
+        
+        bin_list.append(cur_bin)
+    
+    #Histograms are normalized so they can be compared on same scale
+    plt.hist(bin_list, bins='auto', normed=True, label=bin_labels)
+    
+if save_xyz_file:
+    createXyzFile(center_coord, ring_size)
+
+splitRingsIntoBins(250, hole_dist, ring_size)
 
 if display_result:
     #Display image with centers plotted
-    if has_key:
-        image_view = viewer.CollectionViewer([image, key_image])
-    else:
-        image_view = viewer.ImageViewer(image)
+    viewImage(image)
     
     if save_image:
         io.imsave(output_filename, image)
-    image_view.show()
